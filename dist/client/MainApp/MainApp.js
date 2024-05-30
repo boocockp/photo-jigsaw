3e3181963f708331a9d2795b819b874ea4f8f3bd
boocockp <>
2024-05-30 15:25:41 +0100

    Tidy up screen layout

--- dist/client/MainApp/MainApp.js
+++ dist/client/MainApp/MainApp.js
@@ -10,6 +10,8 @@ const MainPage_ChunksItem = React.memo(function MainPage_ChunksItem(props) {
     const {ItemSetItem, Block, Icon} = Elemento.components
     const {If, Eq, Floor} = Elemento.globalFunctions
     const _state = Elemento.useGetStore()
+    const app = _state.useObject('MainApp')
+    const {FileUrl} = app
     const SelectedPiece = _state.useObject(parentPathWith('SelectedPiece'))
     const Width = _state.useObject(parentPathWith('Width'))
     const styles = undefined
@@ -17,7 +19,7 @@ const MainPage_ChunksItem = React.memo(function MainPage_ChunksItem(props) {
     return React.createElement(ItemSetItem, {path: props.path, onClick, styles},
         React.createElement(Block, {path: pathWith('ChunkWrapper'), styles: {height: '100', width: '100', borderColor: If(Eq($itemId, SelectedPiece), 'orange', 'transparent'), borderStyle: 'solid', borderWidth: '3'}},
             React.createElement(Icon, {path: pathWith('OkIndicator'), iconName: 'check', show: Eq($item, $itemId), styles: {color: 'green', backgroundColor: 'white', position: 'absolute', zIndex: 1, bottom: '0', right: '0', fontSize: '16', borderRadius: '50%'}}),
-            React.createElement(Block, {path: pathWith('ImageChunk'), styles: {backgroundImage: 'url(../files/Car1.jpg)', height: '100', width: '100', backgroundColor: 'lightgray', backgroundPositionX: -($item % Width) * 100 + 'px', backgroundPositionY: -Floor($item / Width) * 100 + 'px'}},
+            React.createElement(Block, {path: pathWith('ImageChunk'), styles: {backgroundImage: 'url(' + FileUrl('Car1.jpg') + ')', height: '100', width: '100', backgroundColor: 'lightgray', backgroundPositionX: -($item % Width) * 100 + 'px', backgroundPositionY: -Floor($item / Width) * 100 + 'px'}},
 
     ),
     ),
@@ -79,13 +81,13 @@ function MainPage(props) {
         React.createElement(Data, {path: pathWith('Moves'), display: false}),
         React.createElement(Calculation, {path: pathWith('NumberCorrect'), show: false}),
         React.createElement(Calculation, {path: pathWith('AllCorrect'), show: false}),
-        React.createElement(Calculation, {path: pathWith('GameOver'), show: true}),
-        React.createElement(Timer, {path: pathWith('GameTimer'), label: 'Time', show: true}),
+        React.createElement(Calculation, {path: pathWith('GameOver'), show: false}),
+        React.createElement(Timer, {path: pathWith('GameTimer'), label: 'Time', show: false}),
         React.createElement(Image, {path: pathWith('Image1'), source: 'Car1.jpg', show: false}),
         React.createElement(Button, {path: pathWith('Start'), content: 'Start', appearance: 'outline', action: Start_action}),
-        React.createElement(Layout, {path: pathWith('StatusLine'), horizontal: true, wrap: false},
-            React.createElement(TextElement, {path: pathWith('MovesTaken'), styles: {}}, Moves + ' moves'),
-            React.createElement(TextElement, {path: pathWith('TimeLeft'), styles: {}}, Ceiling(TimeLimit - GameTimer.value) + ' seconds remaining'),
+        React.createElement(Layout, {path: pathWith('StatusLine'), horizontal: true, wrap: false, styles: {fontSize: '20px'}},
+            React.createElement(TextElement, {path: pathWith('MovesTaken'), styles: {fontSize: 'inherit'}}, 'Moves: ' +  Moves),
+            React.createElement(TextElement, {path: pathWith('TimeLeft'), styles: {fontSize: 'inherit', marginLeft: '6em'}}, 'Remaining: ' + Ceiling(TimeLimit - GameTimer.value) + ' seconds'),
     ),
         React.createElement(Layout, {path: pathWith('Layout1'), horizontal: true, wrap: true, styles: {width: '530', height: '515', gap: '0'}},
             React.createElement(ItemSet, {path: pathWith('Chunks'), itemContentComponent: MainPage_ChunksItem}),
@@ -98,7 +100,7 @@ export default function MainApp(props) {
     const pathWith = name => 'MainApp' + '.' + name
     const {App} = Elemento.components
     const pages = {MainPage}
-    const {appContext} = props
+    const appContext = Elemento.useGetAppContext()
     const _state = Elemento.useGetStore()
     const app = _state.setObject('MainApp', new App.State({pages, appContext}))
 
